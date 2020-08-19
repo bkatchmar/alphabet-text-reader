425b471df4a66622cf679421b47f8d40f709d903
Brian Katchmar <bkatchmar@gmail.com>
2020-08-18 21:10:05 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace alphabet_text_reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lets pretend there is some fancy dependency injector here or some design pattern like a factory class that generates this
+            ITextCharacterCounter counter;
+
+            /*
+             * Requirement: Receives a string as command line argument
+             * I take this as it is meant to only read 1 string. Since `args` is an array, this means control for only 1 argument
+             */
+            if (args.Length > 0)
+            {
+                string firstArgument = args[0];
+                counter = new AlphabetCharacterCounter();
+                OutputResult(firstArgument, counter);
+            }
+            else
+            {
+                Console.WriteLine("No Arguments Detected; Aborting Operation");
+            }
+        }
+
+        /// <summary>
+        /// I want to handle the output outside of the class as the class should only be used for processing.
+        /// What if instead we output to an MVC app?
+        /// </summary>
+        static void OutputResult(string strToProcess, ITextCharacterCounter counter)
+        {
+            // Initial Variables
+            IDictionary<char,int> processed = counter.ProcessString(strToProcess);
+            var header = new StringBuilder("|");
+            var dataRow = new StringBuilder("|");
+
+            // Loop through the result of `processed` and build the output strings
+            foreach (KeyValuePair<char,int> pair in processed) 
+            {
+                // Build the header for this record
+                header.Append(pair.Key);
+                header.Append("|");
+
+                // Build the row for this record
+                dataRow.Append(pair.Value);
+                dataRow.Append("|");
+            }
+
+            // Output
+            Console.WriteLine(header.ToString());
+            Console.WriteLine(dataRow.ToString());
+
+            // (Optional) After the tabulate is displayed, display the message
+            Console.WriteLine($"The text has been processed. Total letters counted: {counter.TotalNumberOfLettersCounted}");
+        }
+    }
+
+    /// <summary>Interface class for this exercise</summary>
+    /// <remarks>Must be implemented in order to write a custom class solution.</remarks>
+    interface ITextCharacterCounter
+    {
+        char[] CharactersToCount { get; }
+        int TotalNumberOfLettersCounted { get; }
+        IDictionary<char,int> ProcessString(string str);
+    }
+
+    /// <summary>Implementation of ITextCharacterCounter</summary>
+    /// <remarks>Counts only alphabetical characters and disregards case.</remarks>
+    class AlphabetCharacterCounter : ITextCharacterCounter
+    {
+        /// <summary>Class implementation definition of what characters to count</summary>
+        public char[] CharactersToCount
+        {
+            get
+            {
+                return new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            }
+        }
+
+        /// <summary>Counter for how many characters have been counted</summary>
+        public int TotalNumberOfLettersCounted { get; private set; }
+
+        public AlphabetCharacterCounter()
+        {
+            this.TotalNumberOfLettersCounted = 0; // Default value
+        }
+
+        /// <summary>Returns a data structure for each character counted</summary>
+        /// <param name="str">The string to process</param>
+        public IDictionary<char, int> ProcessString(string str)
+        {
+            // This will be built up and eventually returned after the process is complete
+            var rtnVal = new Dictionary<char, int>();
+
+            // Initial setup
+            foreach (char character in this.CharactersToCount) rtnVal.Add(character, 0);
+
+            // It is not relevant for the counting if the letters are capitalized or not
+            str = str.ToUpper();
+            char[] lookup = str.ToCharArray();
+
+            // Loop through char array and increment where a counter can be found
+            foreach (char current in lookup)
+            {
+                if (rtnVal.ContainsKey(current))
+                {
+                    // Increment Dictionary
+                    rtnVal[current]++;
+                    // Increment Total Counter
+                    this.TotalNumberOfLettersCounted++;
+                }
+            }
+
+            return rtnVal;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# alphabet-text-reader
- IntegriDATA Exercise
+# Introduction
+
+IntegriDATA Exercise. The requirements of this are outlined in a PDF file. The PDF itself will be attached to this project.
+
+# About this project
+
+This project was created in Visual Studio Code with .NET Core as a C# Console Application. The first thing done to create this project was the command line:
+
+```
+dotnet new console
+```
+
+The basis of this project means that distribution depends on copying and pasting into an email. Therefore for simplicity only one `Program.cs` file will be developed for this.
+
+The first thing to be noted as this program is designed to accept a string as a command line argument. Therefore, this program is run by commands such as:
+
+```
+dotnet run "pizza"
+dotnet run "chicken" "salad"
+```
+
+In some cases, if more than one argument is provided, its ignored as the requirement to "accept a string" implies only one.
+
+# Structure
+
+While the delivery was communicated as "copy and paste the solution" and I rather have only one file for simplicity, that can be easily copied and pasted, that doesn't mean that I don't get to use other classes or interfaces. With that said, I settled on a simple structure of one interface and a class that implements it.
+
+## interface ITextCharacterCounter
+
+This interface defines what an implementation of this solution will contain. The concept here is what if instead of a solution that requires only alphabetical characters that disregard case, what if instead I want to include case, numbers, or other special characters? So when the console application consumes this solution, depending on the instance of the class, the solution can be independent of the output.
+
+## class AlphabetCharacterCounter
+
+The class used for this particular solution. Only counts alphabetical characters and disregards case. So "A" and "a" will be counted in the same category.
+
+# Running and debugging
+
+Examples that were tested with this are as follows:
+
+```
+dotnet run "pizza" "pepperoni"
+```
+
+I expect "pepperoni" to be ignored and only "Pizza" to be processed.
+
+```
+dotnet run "Cbb acCA"
+dotnet run "numbers 8 and 9"
+```
